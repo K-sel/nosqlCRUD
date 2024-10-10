d5ec4842af447b921c545d667bf03020955bdf06
Vince <jpinard@bluewin.ch>
2024-10-10 14:56:52 +0200

    inital commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import PouchDB from 'pouchdb'
+
+export default {
+  data() {
+    return {
+      datas: [],
+      databaseReference: null as PouchDB.Database | null
+    }
+  },
+
+  methods: {
+    inc() {},
+
+    initDatabase() {
+      const db = new PouchDB('http://admin:admin@localhost:5984/post')
+      if (db) {
+        console.log("Connected to collection 'post'")
+      } else {
+        console.warn('Something went wrong')
+      }
+      this.databaseReference = db
+    },
+
+    fetchData() {
+      // récupérer tous les documents de ma collection
+    }
+  },
+
+  mounted() {
+    console.log('hello')
+  }
+}
+</script>
+
+<template>
+  <h1>InfraDon2</h1>
+  <p>Counter: {{ datas }}</p>
+  <button type="button" role="button" @click="inc">+1</button>
+</template>
